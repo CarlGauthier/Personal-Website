28bbea8f1f1370ef53c58ee085805055d99a4321
Carl Gauthier <gauthier_carl004@hotmail.com>
2017-11-02 18:18:50 -0400

    Work in progress

--- server.js
+++ server.js
@@ -10,21 +10,37 @@ var connection = mysql.createConnection({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     host: process.env.MYSQL_HOST,
-    database : process.env.MYSQL_DATABASE
+    database : process.env.MYSQL_DATABASE,
+    multipleStatements: true
 });
 
 connection.connect(function(err) {
-    console.log(process.env.MYSQL_DATABASE);
     if (err) throw err;
         console.log("Connected!");
     
 });
 
 app.get('/', function(req, res) {
-    QueryAction('index.ejs', 'Select * From ProjectView Limit 5', 'Accueil', res);
+    Action('index.ejs', 'Select * From ProjectView Limit 5; Select * From Context', 'Accueil', res);
 });
 
-function QueryAction(page, query, pageTitle, res) {
+function Action(page, query, pageTitle, res) {
+    connection.query(query, function(err, result) {
+        if(err){
+            throw err;
+        } else {
+            console.log(result.count);
+            //var json = {title: pageTitle}
+            //json.push()
+            //console.log(json);
+            //res.render(page, {title: pageTitle, model: result[0], model2: result[1]});
+        }
+    });
+    connection.end();
+}
+
+/*
+function Action(page, query, pageTitle, res) {
     connection.query(query, function(err, result) {
         if(err){
             throw err;
@@ -33,6 +49,15 @@ function QueryAction(page, query, pageTitle, res) {
             res.render(page, {title: pageTitle, data: result});
         }
     });
+    connection.end();
 }
+*/
 
 app.listen(80);
+
+/*
+var myObj = new Object;
+var a = 'string1';
+myObj[a] = 'whatever';
+alert(myObj.string1)
+*/
